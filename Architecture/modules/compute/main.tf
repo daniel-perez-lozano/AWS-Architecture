4810e52a21cf8a666ec05721cf33a36143ece763
Perez Lozano <deloitte.cbs14@grupoiberostar.com>
2025-01-26 00:10:58 +0100

    Making progess on the hub

--- Architecture/modules/compute/main.tf
+++ Architecture/modules/compute/main.tf
@@ -8,26 +8,6 @@
     Name = "app-instance"
   }
 }*/
-variable "ami" {
-  description = "AMI ID"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "Instance type"
-  type        = string
-}
-
-variable "private_subnets" {
-  description = "List of private subnet IDs"
-  type        = list(string)
-}
-
-variable "key_pair" {
-  description = "Key pair name"
-  type        = string
-}
-
 resource "aws_launch_configuration" "launch_config_app" {
   name_prefix   = "app-launch-configuration"
   image_id      = var.ami
@@ -43,16 +23,62 @@ resource "aws_autoscaling_group" "scalgrpfront_north1a" {
   desired_capacity     = 2
   max_size             = 3
   min_size             = 1
-  vpc_zone_identifier  = var.private_subnets
+  vpc_zone_identifier  = [for subnet in var.private_subnets : subnet if subnet != "10.0.3.0/24" && subnet != "10.0.7.0/24" && subnet != "10.0.6.0/24"]
   launch_configuration = aws_launch_configuration.launch_config_app.id
 
   tag {
     key                 = "Name"
-    value               = "app-instance"
+    value               = "scalgrpfront_north1a"
+    propagate_at_launch = true
+  }
+}
+resource "aws_autoscaling_group" "scalgrpfront_north1b" {
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 1
+  vpc_zone_identifier  = [for subnet in var.private_subnets : subnet if subnet != "10.0.3.0/24" && subnet != "10.0.7.0/24" && subnet != "10.0.6.0/24"]
+  launch_configuration = aws_launch_configuration.launch_config_app.id
+
+  tag {
+    key                 = "Name"
+    value               = "scalgrpfront_north1b"
+    propagate_at_launch = true
+  }
+}
+resource "aws_autoscaling_group" "scalgrpbck_north1a" {
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 1
+  vpc_zone_identifier  = [for subnet in var.private_subnets : subnet if subnet != "10.0.3.0/24" && subnet != "10.0.7.0/24" && subnet != "10.0.6.0/24"]
+  launch_configuration = aws_launch_configuration.launch_config_app.id
+
+  tag {
+    key                 = "Name"
+    value               = "scalgrpbck_north1a"
+    propagate_at_launch = true
+  }
+}
+resource "aws_autoscaling_group" "scalgrpbck_north1b" {
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 1
+  vpc_zone_identifier  = [for subnet in var.private_subnets : subnet if subnet != "10.0.3.0/24" && subnet != "10.0.7.0/24" && subnet != "10.0.6.0/24"]
+  launch_configuration = aws_launch_configuration.launch_config_app.id
+
+  tag {
+    key                 = "Name"
+    value               = "scalgrpbck_north1b"
     propagate_at_launch = true
   }
 }
 
+
 output "autoscaling_group_name" {
-  value = aws_autoscaling_group.scalgrpfront_north1a.name
+  value =[
+    aws_autoscaling_group.scalgrpbck_north1a.name,
+    aws_autoscaling_group.scalgrpbck_north1b.name,
+    aws_autoscaling_group.scalgrpfront_north1b.name,
+    aws_autoscaling_group.scalgrpfront_north1a.name,
+  ] 
+  
 }
