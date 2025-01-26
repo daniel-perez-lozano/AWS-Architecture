8ea358d522ff8339474c3fe1f7d5dade118b3b1c
Perez Lozano <deloitte.cbs14@grupoiberostar.com>
2025-01-26 21:26:30 +0100

    struggling with eks

--- IaC/kubernetes.tf
+++ IaC/kubernetes.tf
@@ -1,9 +1,8 @@
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = module.eks.cluster_token
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
-
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
